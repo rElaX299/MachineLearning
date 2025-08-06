c0a5425ed8947a458c04d52a9e8c83725407f292
Rex <2399984023@qq.com>
2025-08-06 22:05:23 +0800

    <refactor>cleancode Date:20250806 Author:Rex Scope: Description: Footer:

--- machine_learning/Chapter01/Quiz01.py
+++ machine_learning/Chapter01/Quiz01.py
@@ -1,24 +1,25 @@
 # 1.1
+from typing import Tuple, List
 from dataclasses import dataclass
 from enum import Enum, unique
 
 @unique
 class Color(Enum):
-    green = 0
-    black = 1
-    na = 9
+    GREEN = 0x0
+    BLACK = 0x1
+    NA = 0xff
 
 @unique
 class Root(Enum):
-    curled = 0
-    twisted = 1
-    na = 9
+    CURLED = 0x0
+    TWISTED = 0x1
+    NA = 0xff
 
 @unique
 class Voice(Enum):
-    muffled = 0
-    murky = 1
-    na = 9
+    MUFFLED = 0x0
+    MURKY = 0x1
+    NA = 0xff
 
 @dataclass
 class WatermelonStatus:
@@ -31,32 +32,42 @@ class Watermelon:
     status: WatermelonStatus
     is_good: bool
 
-def solution():
-    good_watermelon1 = Watermelon(WatermelonStatus(
-        Color.green, Root.curled, Voice.murky), True)
-    bad_watermelon1 = Watermelon(WatermelonStatus(
-        Color.black, Root.twisted, Voice.muffled), False)
-    count = 0
-    result = []
-    for color in Color:
-        if color == bad_watermelon1.status.color:
-            continue
-        for root in Root:
-            if root == bad_watermelon1.status.root:
-                continue
-            for voice in Voice:
-                if voice == bad_watermelon1.status.voice:
+# 题目条件
+good_watermelon1 = Watermelon(WatermelonStatus(
+    Color.GREEN, Root.CURLED, Voice.MURKY), True)
+bad_watermelon1 = Watermelon(WatermelonStatus(
+    Color.BLACK, Root.TWISTED, Voice.MUFFLED), False)
+
+# 解决方案
+def solution() -> Tuple[int, List]:
+    version_space_size = 0
+    version_space = []
+
+    def match_good_watermelon(color, root, voice):
+        is_match = color == good_watermelon1.status.color or \
+                   root == good_watermelon1.status.root or \
+                   voice == good_watermelon1.status.voice
+        return is_match
+
+    positive_colors = [c for c in Color if c != bad_watermelon1.status.color]
+    positive_roots = [r for r in Root if r != bad_watermelon1.status.root]
+    positive_voices = [v for v in Voice if v != bad_watermelon1.status.voice]
+
+    for c in positive_colors:
+        for r in positive_roots:
+            for v in positive_voices:
+                if not match_good_watermelon(c, r, v):
                     continue
-                if (color != good_watermelon1.status.color and 
-                    root != good_watermelon1.status.root and 
-                    voice != good_watermelon1.status.voice):
-                    continue
-                count += 1
-                tmp = (color.name, root.name, voice.name)
-                result.append(tmp)
-    print(count)
-    for item in result:
-        print(item)
+                version_space_size += 1
+                hypothesis = (c.name, r.name, v.name)
+                version_space.append(hypothesis)
+
+    return version_space_size, version_space
+
 
 if __name__ == "__main__":
-    solution()
+    size, space = solution()
+
+    print(size)
+    for item in space:
+        print(item)
